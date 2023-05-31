c7e081f0ce5df138efb6b8074fadb7fc65eecda8
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2023-05-30 23:52:42 -0300

    Cadastrado Médias

--- src/src/ClassesModel/Aluno.java
+++ src/src/ClassesModel/Aluno.java
@@ -11,7 +11,13 @@ public class Aluno {
     String dataMatricula;
     String nomeEscola;
 
-    public Aluno(String nome, int idade, String dataNascimento, String numeroCpf, String nomePai, String nomeMae, String dataMatricula, String nomeEscola) {
+    private Double nota1;
+    private Double nota2;
+    private Double nota3;
+    private Double nota4;
+
+
+    public Aluno(String nome, int idade, String dataNascimento, String numeroCpf, String nomePai, String nomeMae, String dataMatricula, String nomeEscola, Double nota1, Double nota2, Double nota3, Double nota4) {
         this.nome = nome;
         this.idade = idade;
         this.dataNascimento = dataNascimento;
@@ -20,6 +26,10 @@ public class Aluno {
         this.nomeMae = nomeMae;
         this.dataMatricula = dataMatricula;
         this.nomeEscola = nomeEscola;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
     }
 
     public String getNome() {
@@ -84,6 +94,42 @@ public class Aluno {
 
     public void setNomeEscola(String nomeEscola) {
         this.nomeEscola = nomeEscola;
+    }
+
+    public Double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(Double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public Double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(Double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public Double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(Double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public Double getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(Double nota4) {
+        this.nota4 = nota4;
+    }
+
+    public double getMediaNota()  {
+        return (nota1 + nota2 + nota3 + nota4) /4;
     }
 
     @Override
--- src/src/cadastros/cadastroAluno.java
+++ src/src/cadastros/cadastroAluno.java
@@ -5,12 +5,16 @@ import javax.swing.*;
 
 public class cadastroAluno {
     public static void cadastrar(){
-        Aluno aluno1 = new Aluno("Jose",23,"23-02-1993","3213-123","Joao","Maria","23-02-2020","Satc");
+        Aluno aluno1 = new Aluno("Jose",23,"23-02-1993","3213-123","Joao","Maria","23-02-2020","Satc",5.5,6.6,4.5,9.0);
 
-        int resposta = JOptionPane.showConfirmDialog(null," Deseja ver o aluno cadastrado ?");
+        int resposta = JOptionPane.showConfirmDialog(null," Deseja ver o aluno cadastrado e sua média ?");
 
         if (resposta == 0){
-            JOptionPane.showMessageDialog(null," SIM " + aluno1);
+            JOptionPane.showMessageDialog(null," SIM!! \n " +
+                    " Aluno "+ aluno1 +
+                    "\n Média: " + aluno1.getMediaNota()+
+                    "\n Nota 1: " +aluno1.getNota1()
+            );
         }else if (resposta == 1){
             JOptionPane.showMessageDialog(null, "NÃO");
         } else if (resposta == 2) {
@@ -18,4 +22,6 @@ public class cadastroAluno {
         }
 
     }
+
+
 }
